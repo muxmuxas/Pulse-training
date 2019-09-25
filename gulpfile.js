c3d5c66e46b764b6441c51166bae3d311fc79e2f
Mykhaylo <kobylyukh@gmail.com>
2019-09-25 23:10:34 +0300

    Optimized

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const sass = require('gulp-sass');
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -22,18 +24,37 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('js', function() {
-    return gulp.src("src/js/*.js")
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("src/js/*.js", gulp.parallel('js'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/js/*.js", gulp.parallel('scripts'));
+    gulp.watch("src/*.html").on("change", gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('img', function() {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'img', 'html'));
